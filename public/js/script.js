0d0cfe8fbef1cd68918d795f3e5cb05c0c18d8f1
julien dewerpe <julien.dewerpe@orange.fr>
2023-04-06 23:37:10 +0200

    verification plugboard

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
+var isValidInput = function(input){
+    return /^[a-zA-Z]*$/g.test(input);
+}
+
+var isAlphabeticChar = function(input){
+    return /^[a-zA-Z]*$/g.test(input);
+}
+
+var isDifferentChar = function(input, current){
+    return (input !== current);
+}
+
+var isValidPlugboardConfig = function(input, current){
+    return (isAlphabeticChar(input) && isDifferentChar(current));
+}
+
+var getPlugboardConfig = function() {
+    var pairs = $('.plugboard').map(function() {
+        thisLetter = $(this).attr('id').slice(-1);
+        otherLetter = $(this).val().toUpperCase();
+
+        if(otherLetter) {
+            return (thisLetter + otherLetter);
+        }
+    }).get();
+
+    return pairs;
+}
+
 $('#input').keyup(function(e) {
     input = $(this).val();
     validInput = input.replace(/([^a-zA-Z\s]+)/gi, '').toUpperCase();
