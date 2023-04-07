d733d6ac00955d8889cc3ede4b96b9d190ca47a7
julien dewerpe <julien.dewerpe@orange.fr>
2023-04-07 16:26:17 +0200

    récuperation de la configuration utilisateur du plugboard

--- public/js/script.js
+++ public/js/script.js
@@ -27,6 +27,38 @@ var getPlugboardConfig = function() {
     return pairs;
 }
 
+$('.plug-settings').keydown(function(e) {
+    e.preventDefault();
+
+    thisLetter = $(this).attr('id').slice(-1);
+    otherLetter = String.fromCharCode(e.keyCode).toUpperCase();
+    previousLetter = $(this).attr('previousLetter');
+
+    // Erase any other letter that is currently connected to the
+    // letter we want right now
+    if (isAlphabeticChar(otherLetter)) {
+        currentOtherLetter = $('#plug-' + otherLetter).val();
+        if (currentOtherLetter)
+            $('#plug-' + currentOtherLetter).val('');
+    }
+
+    // Erasing the letter that we were previously connected to
+    if (previousLetter)
+        $('#plug-' + previousLetter).val('');
+
+    if (isValidPlugboardConfig(otherLetter, thisLetter)) {
+        $(this).val(otherLetter);
+        $(this).attr('previousLetter', otherLetter);
+        $('#plug-' + otherLetter).val(thisLetter);
+        $('#plug-' + otherLetter).attr('previousLetter', thisLetter);
+    } else {
+        $(this).attr('previousLetter', '');
+        $(this).val('');
+    }
+
+    updateOutput();
+});
+
 $('#input').keyup(function(e) {
     input = $(this).val();
     validInput = input.replace(/([^a-zA-Z\s]+)/gi, '').toUpperCase();
